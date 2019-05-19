2f65b67595de6ce682f658521c63d3a9f085cd46
David Harabagiu <harabagiudavid@gmail.com>
2019-05-19 21:36:51 +0300

    Registered KeyStorageController QML type

--- helios-client/controllers/src/controllers.cpp
+++ helios-client/controllers/src/controllers.cpp
@@ -4,10 +4,12 @@
 #include "qauthenticationcontroller.h"
 #include "qsettingscontroller.h"
 #include "qremotefilesystemcontroller.h"
+#include "qkeystoragecontroller.h"
 
 void Controllers::registerQmlTypes(const char* uri)
 {
     qmlRegisterType<QAuthenticationController>(uri, 1, 0, "AuthenticationController");
     qmlRegisterType<QSettingsController>(uri, 1, 0, "SettingsController");
     qmlRegisterType<QRemoteFileSystemController>(uri, 1, 0, "RemoteFileSystemController");
+    qmlRegisterType<QKeyStorageController>(uri, 1, 0, "KeyStorageController");
 }
